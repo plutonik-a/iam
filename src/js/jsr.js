123db76e6c1ffc5a45bde42e50d54dc077f9ff7a
Plutonik <plutonik@gmx.net>
2015-05-16 01:25:43 +0200

    Change eventHandler for showing tooltip in 'image object'

--- src/js/jsr.js
+++ src/js/jsr.js
@@ -1,295 +1,1 @@
-/**
- * @authors Jörn Kreutel,
- * Alexandra von Criegern, https://github.com/plutonik-a
- */
-
-var zeitdokumente,
-    einfuehrungstext,
-    verknuepfungen,
-    excerpt,
-    objekt,
-    medienverweise,
-    merkliste,
-    tooltip;
-
-var byId = function (id) {
-    return document.getElementById(id);
-}
-
-var bySelector = function (selector) {
-    return document.querySelector(selector);
-}
-
-var hide = function (element) {
-    element.classList.toggle('hidden', true)
-};
-var show = function (element) {
-    element.classList.toggle('hidden', false)
-};
-
-
-/*
- * the following vars and functions serve for switching between different configs and shall be used for realising the jsr exercise
- */
-var configs = ["/content/data/layout_jsr.json", "/content/data/all-items.json", "/content/data/der_bau.json", "/content/data/uebungen/layout_1.json", "/content/data/uebungen/layout_2.json", "/content/data/uebungen/layout_3.json"];
-var currentConfigId = 0;
-
-function switchJsonConfig() {
-    if (currentConfigId < (configs.length - 1)) {
-        currentConfigId = currentConfigId + 1;
-    } else {
-        currentConfigId = 0;
-    }
-}
-
-var config_switch;
-
-/*
- * this functions loads the content given the selected config
- */
-function initialiseView() {
-    zeitdokumente = byId("zeitdokumente");
-    einfuehrungstext = byId("einfuehrungstext");
-    verknuepfungen = byId("verknuepfungen");
-    objekt = bySelector(".object");
-    medienverweise = bySelector(".mediaboxes");
-    excerpt = bySelector(".excerpt");
-    merkliste = bySelector(".list");
-    tooltip = byId("selectedObject");
-
-    // set the switch function as onlick on the config_switch element and display the config
-    config_switch = document.getElementById("config_switch");
-    config_switch.onclick = function () {
-        switchJsonConfig();
-        loadContentAndCreateLayout();
-    }
-    loadContentAndCreateLayout();
-}
-
-function removeAllMediaboxes() {
-    var mediaboxes = document.querySelectorAll('.mediabox');
-    var i = 0;
-    var l = mediaboxes.length;
-    for (; i < l; i++) {
-        medienverweise.removeChild(mediaboxes[i]);
-    }
-}
-
-function loadContentAndCreateLayout() {
-    // we display the current config
-    var pathSegments = configs[currentConfigId].split("/");
-    config_switch.textContent = pathSegments[pathSegments.length - 1];
-
-    // we reset the layout, setting all elements to hidden, first
-    hide(zeitdokumente);
-    hide(einfuehrungstext);
-    hide(verknuepfungen);
-    hide(excerpt);
-    hide(objekt);
-    hide(merkliste);
-
-    removeAllMediaboxes();
-    console.log("loadContentFromServer()");
-
-    // we load the json data that contains the content that will be used to populate the view elements
-    xhr("GET", ((currentConfigId && configs) ? configs[currentConfigId] : "/content/data/layout_jsr.json"), null, function (xmlhttp) {
-        // we read out the textual content from the response, parsing it as json -- try out with
-        var textContent = xmlhttp.responseText;
-        console.log("responseText from server is: " + textContent);
-        var jsonContent = JSON.parse(textContent);
-        console.log("responseText as json object is: " + jsonContent);
-
-        // we read out the title and set it
-        setTitle(jsonContent.title);
-
-        // the content is a list of json objects. log its length
-        console.log("length of content items loaded from server is: " + jsonContent.content_items.length);
-
-        // now iterate over the items checking its type and calling the appropriate function for creating the content item
-        for (var i = 0; i < jsonContent.content_items.length; i++) {
-            var currentItem = jsonContent.content_items[i];
-            // log the item type
-            console.log("type of item is: " + currentItem.type);
-            switch (currentItem.type) {
-                case "zeitdokumente":
-                    createZeitdokumente(currentItem);
-                    break;
-                case "einfuehrungstext":
-                    createEinfuehrungstext(currentItem);
-                    break;
-                case "textauszug":
-                    createTextauszug(currentItem);
-                    break;
-                case "verknuepfungen":
-                    createVerknuepfungen(currentItem);
-                    break;
-                case "objekt":
-                    createObject(currentItem);
-                    break;
-                case "merkliste":
-                    createMerkliste(currentItem);
-                    break;
-                case "medienverweise":
-                    createMedienverweise(currentItem);
-                    break;
-                default:
-                    console.log("cannot handle item type: " + currentItem.type + ". Ignore for the time being...");
-            }
-        }
-    });
-}
-
-function setTitle(title) {
-    console.log("setTitle(): " + title);
-    title = document.getElementById("topic_title").textContent;
-}
-
-/**
- *
- * @param contentItem
- */
-function createZeitdokumente(contentItem) {
-    console.log("createZeitdokumente()");
-    // set hidden to false
-    show(zeitdokumente);
-    // we set the src attribute of the img
-    //document.querySelector("#zeitdokumente img").setAttribute("src", contentItem.src);
-    zeitdokumente.querySelector("img").setAttribute("src", contentItem.src);
-    // ... and the caption
-    zeitdokumente.getElementsByTagName("figcaption")[0].textContent = contentItem.title;
-}
-
-/**
- *
- * @param contentItem
- */
-function createObject(contentItem) {
-    console.log("createObject()");
-    show(objekt);
-    // we set the src attribute of the img
-    objekt.querySelector("img").setAttribute("src", contentItem.src);
-    tooltip.addEventListener('click', function() {
-        alert(contentItem.description);
-    });
-}
-
-/**
- *
- * @param contentItem
- */
-function createMerkliste(contentItem) {
-    console.log("createMerkliste()");
-    show(merkliste);
-}
-
-/**
- *
- * @param contentItem
- */
-function createEinfuehrungstext(contentItem) {
-    console.log("createEinfuehrungstext()");
-    show(einfuehrungstext);
-
-    // the content will be provided by a server-side html file which we set as innerHTML in the local attachment site (the div element marked as "contentfragment")
-    xhr("GET", contentItem.src, null, function (xmlhttp) {
-        console.log("received response for textauszug", xmlhttp.responseText);
-        einfuehrungstext.querySelector(".contentfragment").innerHTML = xmlhttp.responseText;
-    });
-}
-
-/**
- *
- * @param contentItem
- */
-function createTextauszug(contentItem) {
-    console.log("createTextauszug()");
-    show(excerpt);
-
-    // the content will be provided by a server-side html file which we set as innerHTML in the local attachment site (the div element marked as "contentfragment")
-    xhr("GET", contentItem.src, null, function (xmlhttp) {
-        console.log("received response for textauszug", xmlhttp.responseText);
-        excerpt.querySelector(".contentfragment").innerHTML = xmlhttp.responseText;
-    });
-}
-
-/**
- *
- * @param contentItem
- */
-/* here, we demonstrate how to create an element from scratch using the dom api (rather than concatenating a string that might be set as innerHTML as the content above) */
-function createVerknuepfungen(contentItem) {
-    console.log("createVerknuepfungen()");
-    show(verknuepfungen);
-
-    // we read out the list element
-    var ul = verknuepfungen.querySelector("ul");
-    console.log("found ul: " + ul);
-
-    // we clear the element which might contain child elements: 'as long as a firstChild can be found underneath of the element, we remove it'
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
-    }
-
-    // then iterate over the list of links that is contained in the contentItem object
-    for (var i = 0; i < contentItem.content.length; i++) {
-        // show the stepwise rendering of the elements
-        // create a li element
-        li = document.createElement("li");
-        // create an a element and add it as a child to li
-        a = document.createElement("a");
-        li.appendChild(a);
-        // set the target from the current list element as href
-        a.href = contentItem.content[i].target;
-        // and set the title as textContent
-        a.textContent = contentItem.content[i].title;
-
-        // append the complete li element as child to ul
-        ul.appendChild(li);
-    }
-}
-
-/**
- *
- * @param item
- */
-function createMedienverweise(item) {
-    console.log("createMedienverweise()");
-
-    // Box bauen
-    var box = document.createElement('article');
-    box.classList.add('mediabox', 'box', 'media');
-
-    // Titel bauen
-    var headline = document.createElement('h2');
-    var link = document.createElement('a');
-    var arrow = document.createElement('i');
-    var text = document.createTextNode(item.title);
-
-    link.appendChild(text);
-    link.appendChild(arrow);
-    headline.appendChild(link);
-    box.appendChild(headline);
-
-
-    // Liste bauen
-    var ul = document.createElement('ul');
-    box.appendChild(ul);
-    // Listenelemente bauen
-    // then iterate over the list of links that is contained in the contentItem object
-    for (var i = 0; i < item.content.length; i++) {
-        // show the stepwise rendering of the elements
-        // create a li element
-        li = document.createElement("li");
-        // create an a element and add it as a child to li
-        a = document.createElement("a");
-        li.appendChild(a);
-        // set the target from the current list element as href
-        a.href = item.content[i].target;
-        // and set the title as textContent
-        a.textContent = item.content[i].title;
-        // append the complete li element as child to ul
-        ul.appendChild(li);
-    }
-    medienverweise.appendChild(box);
-}
-
+/** * @authors Jörn Kreutel, * Alexandra von Criegern, https://github.com/plutonik-a */var zeitdokumente,    einfuehrungstext,    verknuepfungen,    excerpt,    objekt,    medienverweise,    merkliste,    tooltip;var byId = function (id) {    return document.getElementById(id);}var bySelector = function (selector) {    return document.querySelector(selector);}var hide = function (element) {    element.classList.toggle('hidden', true)};var show = function (element) {    element.classList.toggle('hidden', false)};/* * the following vars and functions serve for switching between different configs and shall be used for realising the jsr exercise */var configs = ["/content/data/layout_jsr.json", "/content/data/all-items.json", "/content/data/der_bau.json", "/content/data/uebungen/layout_1.json", "/content/data/uebungen/layout_2.json", "/content/data/uebungen/layout_3.json"];var currentConfigId = 0;function switchJsonConfig() {    if (currentConfigId < (configs.length - 1)) {        currentConfigId = currentConfigId + 1;    } else {        currentConfigId = 0;    }}var config_switch;/* * this functions loads the content given the selected config */function initialiseView() {    zeitdokumente = byId("zeitdokumente");    einfuehrungstext = byId("einfuehrungstext");    verknuepfungen = byId("verknuepfungen");    objekt = bySelector(".object");    medienverweise = bySelector(".mediaboxes");    excerpt = bySelector(".excerpt");    merkliste = bySelector(".list");    tooltip = byId("selectedObject");    // set the switch function as onlick on the config_switch element and display the config    config_switch = document.getElementById("config_switch");    config_switch.onclick = function () {        switchJsonConfig();        loadContentAndCreateLayout();    }    loadContentAndCreateLayout();}function removeAllMediaboxes() {    var mediaboxes = document.querySelectorAll('.mediabox');    var i = 0;    var l = mediaboxes.length;    for (; i < l; i++) {        medienverweise.removeChild(mediaboxes[i]);    }}function loadContentAndCreateLayout() {    // we display the current config    var pathSegments = configs[currentConfigId].split("/");    config_switch.textContent = pathSegments[pathSegments.length - 1];    // we reset the layout, setting all elements to hidden, first    hide(zeitdokumente);    hide(einfuehrungstext);    hide(verknuepfungen);    hide(excerpt);    hide(objekt);    hide(merkliste);    removeAllMediaboxes();    console.log("loadContentFromServer()");    // we load the json data that contains the content that will be used to populate the view elements    xhr("GET", ((currentConfigId && configs) ? configs[currentConfigId] : "/content/data/layout_jsr.json"), null, function (xmlhttp) {        // we read out the textual content from the response, parsing it as json -- try out with        var textContent = xmlhttp.responseText;        console.log("responseText from server is: " + textContent);        var jsonContent = JSON.parse(textContent);        console.log("responseText as json object is: " + jsonContent);        // we read out the title and set it        setTitle(jsonContent.title);        // the content is a list of json objects. log its length        console.log("length of content items loaded from server is: " + jsonContent.content_items.length);        // now iterate over the items checking its type and calling the appropriate function for creating the content item        for (var i = 0; i < jsonContent.content_items.length; i++) {            var currentItem = jsonContent.content_items[i];            // log the item type            console.log("type of item is: " + currentItem.type);            switch (currentItem.type) {                case "zeitdokumente":                    createZeitdokumente(currentItem);                    break;                case "einfuehrungstext":                    createEinfuehrungstext(currentItem);                    break;                case "textauszug":                    createTextauszug(currentItem);                    break;                case "verknuepfungen":                    createVerknuepfungen(currentItem);                    break;                case "objekt":                    createObject(currentItem);                    break;                case "merkliste":                    createMerkliste(currentItem);                    break;                case "medienverweise":                    createMedienverweise(currentItem);                    break;                default:                    console.log("cannot handle item type: " + currentItem.type + ". Ignore for the time being...");            }        }    });}function setTitle(title) {    console.log("setTitle(): " + title);    title = document.getElementById("topic_title").textContent;}/** * * @param contentItem */function createZeitdokumente(contentItem) {    console.log("createZeitdokumente()");    // set hidden to false    show(zeitdokumente);    // we set the src attribute of the img    //document.querySelector("#zeitdokumente img").setAttribute("src", contentItem.src);    zeitdokumente.querySelector("img").setAttribute("src", contentItem.src);    // ... and the caption    zeitdokumente.getElementsByTagName("figcaption")[0].textContent = contentItem.title;}/** * * @param contentItem */function createObject(contentItem) {    console.log("createObject()");    show(objekt);    // we set the src attribute of the img    objekt.querySelector("img").setAttribute("src", contentItem.src);    tooltip.onclick = function() {        alert(contentItem.description);    }}/** * * @param contentItem */function createMerkliste(contentItem) {    console.log("createMerkliste()");    show(merkliste);}/** * * @param contentItem */function createEinfuehrungstext(contentItem) {    console.log("createEinfuehrungstext()");    show(einfuehrungstext);    // the content will be provided by a server-side html file which we set as innerHTML in the local attachment site (the div element marked as "contentfragment")    xhr("GET", contentItem.src, null, function (xmlhttp) {        console.log("received response for textauszug", xmlhttp.responseText);        einfuehrungstext.querySelector(".contentfragment").innerHTML = xmlhttp.responseText;    });}/** * * @param contentItem */function createTextauszug(contentItem) {    console.log("createTextauszug()");    show(excerpt);    // the content will be provided by a server-side html file which we set as innerHTML in the local attachment site (the div element marked as "contentfragment")    xhr("GET", contentItem.src, null, function (xmlhttp) {        console.log("received response for textauszug", xmlhttp.responseText);        excerpt.querySelector(".contentfragment").innerHTML = xmlhttp.responseText;    });}/** * * @param contentItem *//* here, we demonstrate how to create an element from scratch using the dom api (rather than concatenating a string that might be set as innerHTML as the content above) */function createVerknuepfungen(contentItem) {    console.log("createVerknuepfungen()");    show(verknuepfungen);    // we read out the list element    var ul = verknuepfungen.querySelector("ul");    console.log("found ul: " + ul);    // we clear the element which might contain child elements: 'as long as a firstChild can be found underneath of the element, we remove it'    while (ul.firstChild) {        ul.removeChild(ul.firstChild);    }    // then iterate over the list of links that is contained in the contentItem object    for (var i = 0; i < contentItem.content.length; i++) {        // show the stepwise rendering of the elements        // create a li element        li = document.createElement("li");        // create an a element and add it as a child to li        a = document.createElement("a");        li.appendChild(a);        // set the target from the current list element as href        a.href = contentItem.content[i].target;        // and set the title as textContent        a.textContent = contentItem.content[i].title;        // append the complete li element as child to ul        ul.appendChild(li);    }}/** * * @param item */function createMedienverweise(item) {    console.log("createMedienverweise()");    // Box bauen    var box = document.createElement('article');    box.classList.add('mediabox', 'box', 'media');    // Titel bauen    var headline = document.createElement('h2');    var link = document.createElement('a');    var arrow = document.createElement('i');    var text = document.createTextNode(item.title);    link.appendChild(text);    link.appendChild(arrow);    headline.appendChild(link);    box.appendChild(headline);    // Liste bauen    var ul = document.createElement('ul');    box.appendChild(ul);    // Listenelemente bauen    // then iterate over the list of links that is contained in the contentItem object    for (var i = 0; i < item.content.length; i++) {        // show the stepwise rendering of the elements        // create a li element        li = document.createElement("li");        // create an a element and add it as a child to li        a = document.createElement("a");        li.appendChild(a);        // set the target from the current list element as href        a.href = item.content[i].target;        // and set the title as textContent        a.textContent = item.content[i].title;        // append the complete li element as child to ul        ul.appendChild(li);    }    medienverweise.appendChild(box);}
